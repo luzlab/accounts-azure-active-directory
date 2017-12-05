ed7080f213d9b2786fb405decb95305b69601386
Carlo Quinonez <carlo@studiofathom.com>
2017-12-05 10:50:33 -0800

    Fixed typo in userPrincipleName

--- azure_ad.js
+++ azure_ad.js
@@ -3,28 +3,34 @@ Accounts.oauth.registerService('azureAd');
 if (Meteor.isClient) {
     Meteor.loginWithAzureAd = function(options, callback) {
         // support a callback without options
-        if (! callback && typeof options === "function") {
+        if (!callback && typeof options === 'function') {
             callback = options;
             options = null;
         }
 
-        var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
+        var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(
+            callback,
+        );
         AzureAd.requestCredential(options, credentialRequestCompleteCallback);
     };
 } else {
     var fieldsForLoggedInusers = _.map(
         AzureAd.whitelistedFields.concat(['accessToken', 'expiresAt']), // don't publish refresh token
-        function (subfield) { return 'services.azureAd.' + subfield; }
+        function(subfield) {
+            return 'services.azureAd.' + subfield;
+        },
     );
     var fieldsForOtherUsers = _.map(
         // even with autopublish, no legitimate web app should be
         // publishing all users' emails
-        _.without(AzureAd.whitelistedFields, 'mail', 'userPrincipleName'),
-        function (subfield) { return 'services.azureAd.' + subfield; }
+        _.without(AzureAd.whitelistedFields, 'mail', 'userPrincipalName'),
+        function(subfield) {
+            return 'services.azureAd.' + subfield;
+        },
     );
 
     Accounts.addAutopublishFields({
         forLoggedInUser: fieldsForLoggedInusers,
-        forOtherUsers: fieldsForOtherUsers
+        forOtherUsers: fieldsForOtherUsers,
     });
 }
